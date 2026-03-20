95059e9b2a64bd588bf1395e9f0d6b94410972cd
Cadet Clive Tod D. <cadetdlive23@gmail.com>
2026-03-20 04:29:58 -0400

    Ajouter derniere modifications

--- src/main/java/com/example/quiz/controller/QuizController.java
+++ src/main/java/com/example/quiz/controller/QuizController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -77,4 +77,5 @@ public String repondreQuiz(@PathVariable Long id,
     model.addAttribute("pourcentage", pourcentage);
     
     return "resultat";
+}
 }
